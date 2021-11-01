6164b07c17446d3cd024aaa8bdb5fe4ef2720b27
Fabrício Rangel de Sousa <fabricio3176@gmail.com>
2021-10-31 21:41:40 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Componente7 from './componentes/tokyo';
 import Componente8 from './componentes/cancun';
 import Componente9 from './componentes/restinga';
 import Componente10 from './componentes/alagoas';
+import image from './img/instagram.png';
 
 const App = () => {
 
@@ -24,9 +25,16 @@ const App = () => {
 
     return (
         <div className="App">
-            <div class= "menu">
-            <ul>
-                <li><a href="index.html">Inicio</a></li>
+            <header>
+                <div class="instagram">
+                    <a href="#" >
+                        <img id="img" alt="Instagram" src={image} />
+                    </a>
+                </div>
+            </header>
+            <div class="menu">
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
                     <li><a href="#">Companhias</a></li>
                     <li><a href="#">Informações</a></li>
                     <li><a href="#">Sobre</a></li>
@@ -35,99 +43,99 @@ const App = () => {
             </div>
             <div className="Cartoes">
                 <Cartao titulo="Destino a São Paulo"
-                    color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente1 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
 
                 <Cartao titulo="Destino ao Rio de Janeiro"
-                    color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente2 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
 
                 <Cartao titulo="Destino Ribeirão Preto"
-                    color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente3 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
 
                 <Cartao titulo="Destino a Ibiraci"
-                    color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente4 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
 
                 <Cartao titulo="Destino a Paris"
-                    color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente5 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
 
                 <Cartao titulo="Destino a Manaus"
-                    color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente6 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
 
                 <Cartao titulo="Destino a Tokyo"
-                    color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente7 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
 
                 <Cartao titulo="Destino a Cancun"
-                    color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente8 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
 
                 <Cartao titulo="Destino a Restinga"
-                    color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente9 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
 
                 <Cartao titulo="Destino a Alagoas"
-                     color = "#0C1F1D">
+                    color="#0C1F1D">
                     <Componente10 Decrementar={Decrementar} diminuir={diminuir} />
                 </Cartao>
                 <section className="footer1">
-				<div class="formulario">
-					<div class="footer">
-						<section>
-							<h3>Endereço</h3>
-							<p>
-								Rua Dos Aeroportos, nº 0000<br />
-								Parque das Aeronovas<br />
-								Boeing - SP, CEP 14400-000
-							</p>
-						</section>
-					</div>
-					<div class="footer">
-						<section>
-							<h3>Midias Sociais</h3>
-							<p>
-								<a href="#" target="_blank"
-									rel="external" class="social">instagram.com/Aeroporto</a><br />
-								<a
-									href="#" target="_blank"
-									rel="external" class="social">facebook.com/Aeroporto</a>
-							</p>
-						</section>
-					</div>
-					<div class="footer">
-						<section>
-							<h3>Email</h3>
-							<p>
-								<a href="aeroporto@hotmail.com" target="_blank" rel="external" class="social">aeroporto@hotmail.com</a>
-							</p>
-						</section>
-					</div>
-					<div class="footer">
-						<section>
-							<h3>Telefone</h3>
-							<p>
-								(xx) 12345-6789
-							</p>
-						</section>
-					</div>
-				</div>
-			</section>  
+                    <div class="formulario">
+                        <div class="footer">
+                            <section>
+                                <h3>Endereço</h3>
+                                <p>
+                                    Rua Dos Aeroportos, nº 0000<br />
+                                    Parque das Aeronovas<br />
+                                    Boeing - SP, CEP 14400-000
+                                </p>
+                            </section>
+                        </div>
+                        <div class="footer">
+                            <section>
+                                <h3>Midias Sociais</h3>
+                                <p>
+                                    <a href="#" target="_blank"
+                                        rel="external" class="social">instagram.com/Aeroporto</a><br />
+                                    <a
+                                        href="#" target="_blank"
+                                        rel="external" class="social">facebook.com/Aeroporto</a>
+                                </p>
+                            </section>
+                        </div>
+                        <div class="footer">
+                            <section>
+                                <h3>Email</h3>
+                                <p>
+                                    <a href="aeroporto@hotmail.com" target="_blank" rel="external" class="social">aeroporto@hotmail.com</a>
+                                </p>
+                            </section>
+                        </div>
+                        <div class="footer">
+                            <section>
+                                <h3>Telefone</h3>
+                                <p>
+                                    (xx) 12345-6789
+                                </p>
+                            </section>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
-        
+
     )
 }
 
